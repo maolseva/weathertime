ff945fb0b251a860deafb8e3d47077d5834f021e
maolseva <msepulvedv@gmail.com>
2025-05-10 12:51:21 +0800

    Inject the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -45,6 +45,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+
+
+let days= ["Tue","Wed", "Thu", "Fri", "Sat"];
+let forecastHtml = "";
+
+
+days.forEach(function (day) {
+forecastHtml = 
+forecastHtml +
+` <div class="weather-forecast-day">
+  <div class="weather-forecast-date"> ${day} </div>
+  <div class="weather-forecast-icon"> ⛅ </div>
+  <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature"><strong>15°</strong> </div>
+    <div class="weather-forecast-temperature">9°</div>
+  </div>
+</div>
+`;
+});
+
+let forecastElement = document.querySelector ("#forecast");
+forecastElement.innerHTML = forecastHtml ;
+
+}
+
+
 const form = document.querySelector("#search-form");
 if (form) {
   form.addEventListener("submit", handleSearchSubmit);
@@ -52,3 +79,4 @@ if (form) {
 
 const defaultCity = "Perth";
 searchCity(defaultCity);
+displayForecast();
